2e8c58a7bd42d00f4ee48916ac3df5f0c29896bd
Mark Charles <markcharles1980@gmail.com>
2017-02-19 10:39:58 +0000

    Collapse sticky headers to minimum height

--- lib/src/main/java/ca/barrenechea/widget/recyclerview/decoration/StickyHeaderDecoration.java
+++ lib/src/main/java/ca/barrenechea/widget/recyclerview/decoration/StickyHeaderDecoration.java
@@ -1,3 +1,4 @@
+package ca.barrenechea.widget.recyclerview.decoration;
 /*
  * Copyright 2014 Eduardo Barrenechea
  *
@@ -14,15 +15,12 @@
  * limitations under the License.
  */
 
-package ca.barrenechea.widget.recyclerview.decoration;
-
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,179 +28,264 @@ import java.util.Map;
  * A sticky header decoration for android's RecyclerView.
  */
 public class StickyHeaderDecoration extends RecyclerView.ItemDecoration {
-    public static final long NO_HEADER_ID = -1L;
+	public static final long NO_HEADER_ID = -1L;
 
-    private Map<Long, RecyclerView.ViewHolder> mHeaderCache;
+	private Map<Long, ViewHolder> mHeaderCache;
 
-    private StickyHeaderAdapter mAdapter;
+	private StickyHeaderAdapter mAdapter;
 
-    private boolean mRenderInline;
+	private boolean mRenderInline;
 
-    /**
-     * @param adapter
-     *         the sticky header adapter to use
-     */
-    public StickyHeaderDecoration(StickyHeaderAdapter adapter) {
-        this(adapter, false);
-    }
+	/**
+	 * @param adapter
+	 *         the sticky header adapter to use
+	 */
+	public StickyHeaderDecoration(StickyHeaderAdapter adapter) {
+		this(adapter, false);
+	}
 
-    /**
-     * @param adapter
-     *         the sticky header adapter to use
-     */
-    public StickyHeaderDecoration(StickyHeaderAdapter adapter, boolean renderInline) {
-        mAdapter = adapter;
-        mHeaderCache = new HashMap<>();
-        mRenderInline = renderInline;
-    }
+	/**
+	 * @param adapter
+	 *         the sticky header adapter to use
+	 */
+	public StickyHeaderDecoration(StickyHeaderAdapter adapter, boolean renderInline) {
+		mAdapter = adapter;
+		mHeaderCache = new HashMap<>();
+		mRenderInline = renderInline;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-        int position = parent.getChildAdapterPosition(view);
-        int headerHeight = 0;
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+		int position = parent.getChildAdapterPosition(view);
+		int headerHeight = 0;
 
-        if (position != RecyclerView.NO_POSITION
-                && hasHeader(position)
-                && showHeaderAboveItem(position)) {
+		if (position != RecyclerView.NO_POSITION
+			&& hasHeader(position)
+			&& showHeaderAboveItem(position)) {
 
-            View header = getHeader(parent, position).itemView;
-            headerHeight = getHeaderHeightForLayout(header);
-        }
+			View header = getHeader(parent, position).getViewHolder().itemView;
+			headerHeight = getHeaderHeightForLayout(header);
+		}
 
-        outRect.set(0, headerHeight, 0, 0);
-    }
+		outRect.set(0, headerHeight, 0, 0);
+	}
 
-    private boolean showHeaderAboveItem(int itemAdapterPosition) {
-        if (itemAdapterPosition == 0) {
-            return true;
-        }
-        return mAdapter.getHeaderId(itemAdapterPosition - 1) != mAdapter.getHeaderId(itemAdapterPosition);
-    }
+	private boolean showHeaderAboveItem(int itemAdapterPosition) {
+		if (itemAdapterPosition == 0) {
+			return true;
+		}
+		return mAdapter.getHeaderId(itemAdapterPosition - 1) != mAdapter.getHeaderId(itemAdapterPosition);
+	}
 
-    /**
-     * Clears the header view cache. Headers will be recreated and
-     * rebound on list scroll after this method has been called.
-     */
-    public void clearHeaderCache() {
-        mHeaderCache.clear();
-    }
+	/**
+	 * Clears the header view cache. Headers will be recreated and
+	 * rebound on list scroll after this method has been called.
+	 */
+	public void clearHeaderCache() {
+		mHeaderCache.clear();
+	}
 
-    public View findHeaderViewUnder(float x, float y) {
-        for (RecyclerView.ViewHolder holder : mHeaderCache.values()) {
-            final View child = holder.itemView;
-            final float translationX = ViewCompat.getTranslationX(child);
-            final float translationY = ViewCompat.getTranslationY(child);
+	public View findHeaderViewUnder(float x, float y) {
+		for (ViewHolder holder : mHeaderCache.values()) {
+			final View child = holder.getViewHolder().itemView;
+			final float translationX = ViewCompat.getTranslationX(child);
+			final float translationY = ViewCompat.getTranslationY(child);
 
-            if (x >= child.getLeft() + translationX &&
-                    x <= child.getRight() + translationX &&
-                    y >= child.getTop() + translationY &&
-                    y <= child.getBottom() + translationY) {
-                return child;
-            }
-        }
+			if (x >= child.getLeft() + translationX &&
+				x <= child.getRight() + translationX &&
+				y >= child.getTop() + translationY &&
+				y <= child.getBottom() + translationY) {
+				return child;
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    private boolean hasHeader(int position) {
-        return mAdapter.getHeaderId(position) != NO_HEADER_ID;
-    }
+	private boolean hasHeader(int position) {
+		return mAdapter.getHeaderId(position) != NO_HEADER_ID;
+	}
 
-    private RecyclerView.ViewHolder getHeader(RecyclerView parent, int position) {
-        final long key = mAdapter.getHeaderId(position);
+	private ViewHolder getHeader(RecyclerView parent, int position) {
+		final long key = mAdapter.getHeaderId(position);
 
-        if (mHeaderCache.containsKey(key)) {
-            return mHeaderCache.get(key);
-        } else {
-            final RecyclerView.ViewHolder holder = mAdapter.onCreateHeaderViewHolder(parent);
-            final View header = holder.itemView;
+		if (mHeaderCache.containsKey(key)) {
+			return mHeaderCache.get(key);
+		} else {
+			final RecyclerView.ViewHolder holder = mAdapter.onCreateHeaderViewHolder(parent);
+			final View header = holder.itemView;
 
-            //noinspection unchecked
-            mAdapter.onBindHeaderViewHolder(holder, position);
+			//noinspection unchecked
+			mAdapter.onBindHeaderViewHolder(holder, position);
 
-            int widthSpec = View.MeasureSpec.makeMeasureSpec(parent.getMeasuredWidth(), View.MeasureSpec.EXACTLY);
-            int heightSpec = View.MeasureSpec.makeMeasureSpec(parent.getMeasuredHeight(), View.MeasureSpec.UNSPECIFIED);
+			int widthSpec = View.MeasureSpec.makeMeasureSpec(parent.getMeasuredWidth(), View.MeasureSpec.EXACTLY);
+			int heightSpec = View.MeasureSpec.makeMeasureSpec(parent.getMeasuredHeight(), View.MeasureSpec.UNSPECIFIED);
 
-            int childWidth = ViewGroup.getChildMeasureSpec(widthSpec,
-                    parent.getPaddingLeft() + parent.getPaddingRight(), header.getLayoutParams().width);
-            int childHeight = ViewGroup.getChildMeasureSpec(heightSpec,
-                    parent.getPaddingTop() + parent.getPaddingBottom(), header.getLayoutParams().height);
+			int childWidth = ViewGroup.getChildMeasureSpec(widthSpec,
+				parent.getPaddingLeft() + parent.getPaddingRight(), header.getLayoutParams().width);
+			int childHeight = ViewGroup.getChildMeasureSpec(heightSpec,
+				parent.getPaddingTop() + parent.getPaddingBottom(), header.getLayoutParams().height);
 
-            header.measure(childWidth, childHeight);
-            header.layout(0, 0, header.getMeasuredWidth(), header.getMeasuredHeight());
+			header.measure(childWidth, childHeight);
+			header.layout(0, 0, header.getMeasuredWidth(), header.getMeasuredHeight());
 
-            mHeaderCache.put(key, holder);
+			ViewHolder vh = new ViewHolder();
+			vh.setViewHolder(holder);
+			vh.setMaxViewHeight(header.getMeasuredHeight());
 
-            return holder;
-        }
-    }
+			mHeaderCache.put(key, vh);
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void onDrawOver(Canvas canvas, RecyclerView parent, RecyclerView.State state) {
-        final int count = parent.getChildCount();
-        long previousHeaderId = -1;
+			return vh;
+		}
+	}
 
-        for (int layoutPos = 0; layoutPos < count; layoutPos++) {
-            final View child = parent.getChildAt(layoutPos);
-            final int adapterPos = parent.getChildAdapterPosition(child);
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onDrawOver(Canvas canvas, RecyclerView parent, RecyclerView.State state) {
+		final int count = parent.getChildCount();
+		long previousHeaderId = -1;
 
-            if (adapterPos != RecyclerView.NO_POSITION && hasHeader(adapterPos)) {
-                long headerId = mAdapter.getHeaderId(adapterPos);
+		for (int layoutPos = 0; layoutPos < count; layoutPos++) {
+			final View child = parent.getChildAt(layoutPos);
+			final int adapterPos = parent.getChildAdapterPosition(child);
 
-                if (headerId != previousHeaderId) {
-                    previousHeaderId = headerId;
-                    View header = getHeader(parent, adapterPos).itemView;
-                    canvas.save();
+			if (adapterPos != RecyclerView.NO_POSITION && hasHeader(adapterPos)) {
+				long headerId = mAdapter.getHeaderId(adapterPos);
 
-                    final int left = child.getLeft();
-                    final int top = getHeaderTop(parent, child, header, adapterPos, layoutPos);
-                    canvas.translate(left, top);
+				if (headerId != previousHeaderId) {
+					previousHeaderId = headerId;
+					View header = getHeader(parent, adapterPos).getViewHolder().itemView;
+					canvas.save();
 
-                    header.setTranslationX(left);
-                    header.setTranslationY(top);
-                    header.draw(canvas);
-                    canvas.restore();
-                }
-            }
-        }
-    }
+					final int left = child.getLeft();
+					final int top = getHeaderTop(parent, child, header, adapterPos, layoutPos);
 
-    private int getHeaderTop(RecyclerView parent, View child, View header, int adapterPos, int layoutPos) {
-        int headerHeight = getHeaderHeightForLayout(header);
-        int top = ((int) child.getY()) - headerHeight;
-        if (layoutPos == 0) {
-            final int count = parent.getChildCount();
-            final long currentId = mAdapter.getHeaderId(adapterPos);
-            // find next view with header and compute the offscreen push if needed
-            for (int i = 1; i < count; i++) {
-                int adapterPosHere = parent.getChildAdapterPosition(parent.getChildAt(i));
-                if (adapterPosHere != RecyclerView.NO_POSITION) {
-                    long nextId = mAdapter.getHeaderId(adapterPosHere);
-                    if (nextId != currentId) {
-                        final View next = parent.getChildAt(i);
-                        final int offset = ((int) next.getY()) - (headerHeight + getHeader(parent, adapterPosHere).itemView.getHeight());
-                        if (offset < 0) {
-                            return offset;
-                        } else {
-                            break;
-                        }
-                    }
-                }
-            }
+					canvas.translate(left, top);
 
-            top = Math.max(0, top);
-        }
+					header.setTranslationX(left);
+					header.setTranslationY(top);
+					header.draw(canvas);
+					canvas.restore();
+				}
+			}
+		}
+	}
 
-        return top;
-    }
+	private View setHeaderViewHeight(RecyclerView parent, View headerView, int height) {
 
-    private int getHeaderHeightForLayout(View header) {
-        return mRenderInline ? 0 : header.getHeight();
-    }
+		ViewGroup.LayoutParams params = headerView.getLayoutParams();
+		params.height = height;
+		headerView.setLayoutParams(params);
+
+		int widthMeasureSpec = View.MeasureSpec.makeMeasureSpec(parent.getMeasuredWidth(), View.MeasureSpec.EXACTLY);
+		int heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(parent.getMeasuredHeight(), View.MeasureSpec.AT_MOST);
+
+		int childWidth = ViewGroup.getChildMeasureSpec(widthMeasureSpec,
+			parent.getPaddingLeft() + parent.getPaddingRight(), headerView.getLayoutParams().width);
+		int childHeight = ViewGroup.getChildMeasureSpec(heightMeasureSpec,
+			parent.getPaddingTop() + parent.getPaddingBottom(), headerView.getLayoutParams().height);
+
+		headerView.measure(childWidth, childHeight);
+		headerView.layout(0, 0, headerView.getMeasuredWidth(), headerView.getMeasuredHeight());
+
+		return headerView;
+	}
+
+	private int getHeaderTop(RecyclerView parent, View child, View header, int adapterPos, int layoutPos) {
+		int headerHeight = getHeaderHeightForLayout(header);
+		int top = ((int) child.getY()) - headerHeight;
+
+		if (layoutPos == 0) {
+			// Top most header
+
+			final int count = parent.getChildCount();
+			final long currentId = mAdapter.getHeaderId(adapterPos);
+			// find next view with header and compute the offscreen push if needed
+
+			// Loop over adapter children
+			for (int i = 1; i < count; i++) {
+
+				// Establish adapter posiiton of child
+				int adapterPosHere = parent.getChildAdapterPosition(parent.getChildAt(i));
+				if (adapterPosHere != RecyclerView.NO_POSITION) {
+
+					// Get header ID for child
+					long nextId = mAdapter.getHeaderId(adapterPosHere);
+
+					if (nextId != currentId) {
+						// This child has a different header from the previous one.
+						final View next = parent.getChildAt(i);
+
+						// Get Y Position of this child
+						int nextChildYPos = (int) next.getY();
+
+						// Get the next header view holder
+						RecyclerView.ViewHolder nextHeaderVH = getHeader(parent, adapterPosHere).getViewHolder();
+
+						// Get the height of this next header
+						int nextHeaderHeight = nextHeaderVH.itemView.getHeight();
+
+						// Calculate if top header should be pushed off screen.
+						final int offset = nextChildYPos - (headerHeight + nextHeaderHeight);
+
+						if (offset < 0) {
+							// Top header should be pushed off screen by 'offset' amount
+
+							// Check if header can collapse
+							if (headerHeight > header.getMinimumHeight()) {
+								int proposedHeight = headerHeight - Math.abs(offset);
+								int newHeight = Math.max(proposedHeight, header.getMinimumHeight());
+								setHeaderViewHeight(parent, header, newHeight);
+								return 0;
+							}
+
+						} else {
+							// Header collapsed, check if header can be expanded
+							int proposedHeight = headerHeight + Math.abs(offset);
+							int newHeight = Math.min(proposedHeight, getHeader(parent, adapterPosHere).getMaxViewHeight());
+							setHeaderViewHeight(parent, header, newHeight);
+							return 0;
+						}
+						return offset;
+					}
+				}
+			}
+
+			top = Math.max(0, top);
+		}
+
+
+		return top;
+	}
+
+	private int getHeaderHeightForLayout(View header) {
+		return mRenderInline ? 0 : header.getHeight();
+	}
+
+	private static class ViewHolder {
+
+		private RecyclerView.ViewHolder viewHolder;
+		private int maxViewHeight;
+
+		public int getMaxViewHeight() {
+			return maxViewHeight;
+		}
+
+		public void setMaxViewHeight(int maxViewHeight) {
+			this.maxViewHeight = maxViewHeight;
+		}
+
+		public RecyclerView.ViewHolder getViewHolder() {
+			return viewHolder;
+		}
+
+		public void setViewHolder(RecyclerView.ViewHolder viewHolder) {
+			this.viewHolder = viewHolder;
+		}
+	}
 }
